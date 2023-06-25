20a0025ce186f0d1128b716de0587278ced4cac7
Joanna K <joannakleinrm@gmail.com>
2023-06-25 21:11:00 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import { SharedLayout } from "./SharedLayout";
-// import { Home } from "../pages/Home";
-// import { Movies } from "../pages/Movies";
-// import { MovieDetails } from "../pages/MovieDetails";
-// import { MovieCast } from "./MovieCast";
-// import { MovieReviews } from "./MovieReviews";
-// import { NotFound } from "../pages/NotFound";
+import { Home } from "../pages/Home";
+import { Movies } from "../pages/Movies";
+import { MovieDetails } from "../pages/MovieDetails";
+import { MovieCast } from "./MovieCast";
+import { MovieReviews } from "./MovieReviews";
+import { NotFound } from "../pages/NotFound";
 import { Suspense, lazy } from "react";
 
-export const Home = lazy(() => import("../pages/Home"));
-export const Movies = lazy(() => import("../pages/Movies"));
-export const MovieDetails = lazy(() => import("../pages/MovieDetails"));
-export const MovieCast = lazy(() => import("../components/MovieCast"));
-export const MovieReviews = lazy(() => import("../components/MovieReviews"));
-export const NotFound = lazy(() => import("../pages/NotFound"));
+// export const Home = lazy(() => import("../pages/Home"));
+// export const Movies = lazy(() => import("../pages/Movies"));
+// export const MovieDetails = lazy(() => import("../pages/MovieDetails"));
+// export const MovieCast = lazy(() => import("../components/MovieCast"));
+// export const MovieReviews = lazy(() => import("../components/MovieReviews"));
+// export const NotFound = lazy(() => import("../pages/NotFound"));
 
 
 export const App = () => {
