6a8bf8f537514643f20652ffc13a37ebd1d5b77c
Joanna K <joannakleinrm@gmail.com>
2023-06-28 00:27:17 +0200

    css update

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -21,38 +21,45 @@ export const MovieDetails = () => {
   // const paramsId = Number(params.movieId)
   // const [movieDetails, setMovieDetails] = useState();
   return (
-     <div>
+     <div className={styles.page_container}>
       <ButtonBack />
-      <h2>
-        {movieDetails.title} (
-        {movieDetails.release_date
-          ? movieDetails.release_date.substring(0, 4)
-          : ''}
-        )
-      </h2>
-
-      <img
+      <div className={styles.container}>
+      <img className={styles.img}
         src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${movieDetails.poster_path}`}
         alt="{movieDetails.original_title}"
-      />
+        />
+        <div className={styles.container_details}>
+          <h2 className={styles.title}>
+            {movieDetails.title} (
+            {movieDetails.release_date
+              ? movieDetails.release_date.substring(0, 4)
+              : ''}
+            )
+          </h2>
 
-      <p>
-        User Score:{' '}
-        {movieDetails.vote_average
-          ? Math.fround(movieDetails.vote_average * 10).toFixed(0)
-          : ''}
-        %
-      </p>
-      <h3>Overview</h3>
-      <p>{movieDetails.overview}</p>
+          <p>
+            User Score:{' '}
+            {movieDetails.vote_average
+              ? Math.fround(movieDetails.vote_average * 10).toFixed(0)
+              : ''}
+            %
+          </p>
+          <h3>Overview</h3>
+          <p>{movieDetails.overview}</p>
+          
 
-      <h4>Genres</h4>
-      <p>
-        {movieDetails.genres
-          ? movieDetails.genres.map(genre => genre.name).join(' ')
-          : ''}
-      </p>
+          <h4>Genres</h4>
+          <p>
+            {movieDetails.genres
+              ? movieDetails.genres.map(genre => genre.name).join(' ')
+              : ''}
+          </p>
+        </div>
+
+      </div>
+
       <p>Additional information</p>
+
         <ul className={styles.list}>
           <li key={nanoid()}>
             <Link to="cast" state={{ from: location.state.from }}>
@@ -67,16 +74,7 @@ export const MovieDetails = () => {
         </ul>
         <Outlet />
     </div>
-
-    // <p>Additional information</p>
-    //     <ul className={styles.list} >
-    //       <li key={nanoid()}>
-    //         <Link to="cast" state={{ from: location.state.from }}>Cast</Link></li>
-    //       <li key={nanoid()}>
-    //         <Link to="reviews" state={{ from: location.state.from }}>Reviews</Link></li>
-    //     </ul>
   );
-
 }
 
     // return (
