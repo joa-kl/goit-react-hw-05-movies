1b753446257115a18cd71d52d3c8ec1f5c821e01
joa-kl <113099078+joa-kl@users.noreply.github.com>
2023-06-27 23:33:07 +0000

    Deploying to gh-pages from @ joa-kl/goit-react-hw-05-movies@b923dd94f594849d833398dc0076a632d8bb4ba4 🚀

---
+++ static/js/256.8bf793b8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[256],{256:function(e,t,n){n.r(t),n.d(t,{NotFound:function(){return s}});var o=n(791),r=n(689),u=n(87),c=n(184),s=function(){var e=(0,r.s0)();return(0,o.useEffect)((function(){e("/")}),[]),(0,c.jsxs)("p",{children:["Page is not found. Go ",(0,c.jsx)(u.rU,{to:"/",children:"Home"})]})};t.default=s}}]);
+//# sourceMappingURL=256.8bf793b8.chunk.js.map
